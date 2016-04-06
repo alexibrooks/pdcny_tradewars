5683151dbab0a5249337e4a86ecda7d74c3f0f34
Vishesh Kumar <vishesh@visheshk.net>
2016-04-06 18:21:15 -0500

    Super insecure, most things work!
    You can send requests, trade things.
    Ideally you shouldn’t be able to trade more than you have, though
    that’s not much tested yet.
    You need to run console queries in meteor mongo to setup things.

---
+++ setup-console-queries.js
@@ -0,0 +1,28 @@
+Spammy Queries
+
+us = ["Sit3k7DZdPQs24shx", "QsgMNhnLDPZPRSyAh", "CEiRrYbFaECEzXdTQ", "4ELpfrYfwex6DoEfD"]
+for (u in us){
+	db.users.update({"_id": us[u]}, {$set: {"profile.alerts": []}})
+}
+
+db.users.update({}, {$set: {"profile.alerts": []}});
+
+resources = ["a", "b", "c", "d"]
+groupIDs = ["g1", "g2", "g3", "g4"];
+for (g in groupIDs){
+	print("adding for ", groupIDs[g]);
+	for (r in resources){
+		print("adding ", resources[r]);
+		AllStocks.insert({
+			"gID": groupIDs[g],
+			"item": resources[r],
+			"price": 150,
+			"amount": 50
+		});
+	}
+}
+
+db.AllStocks.find({})
+
+while(All)
+
